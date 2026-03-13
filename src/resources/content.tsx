90c656b633f080d31c6538742b85d75d395f78dd
Jerry Qiu <jqiu61@stuy.edu>
2026-03-13 16:56:11 -0400

    made changes to project cards and pill on homepage

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -59,14 +59,14 @@ const home: Home = {
     display: true,
     title: (
       <Row gap="12" vertical="center">
-        <strong className="ml-4">Once UI</strong>{" "}
+        <strong className="ml-4">Featured</strong>{" "}
         <Line background="brand-alpha-strong" vert height="20" />
         <Text marginRight="4" onBackground="brand-medium">
-          Featured work
+          Recent projects
         </Text>
       </Row>
     ),
-    href: "/work/building-once-ui-a-customizable-design-system",
+    href: "/work",
   },
   subline: (
     <>
